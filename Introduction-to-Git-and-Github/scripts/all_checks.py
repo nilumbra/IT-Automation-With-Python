baf5a6a6c4eacb61986c6c3c9fab11804bb59d70
Mike Chen(HuaXiao) <478222482@qq.com>
2022-07-23 04:45:43 +0900

    Add call to disk_full function

--- Introduction-to-Git-and-Github/scripts/all_checks.py
+++ Introduction-to-Git-and-Github/scripts/all_checks.py
@@ -10,6 +10,9 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+    if disk_full():
+        print("Disk is full.")
+        sys.exit(1)
     print("Everything ok.")
     sys.exit(0)
 
